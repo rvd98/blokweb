3446d165f32a3da4f38d2807f62a24a5754cba52
Rick van Dijken <90160841+rvd98@users.noreply.github.com>
2021-09-29 02:19:36 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
-// JavaScript Document
+console.log("wsup wsup wsup man");
+// HAMBURGER MENU OPENEN
+var hamburgerButton = document.querySelector("header button");
+
+function klapUit(){
+    let deNav = document.querySelector("nav");
+
+    deNav.classList.add("toonNav");
+}
+hamburgerButton.addEventListener("click", klapUit);
+
+// HAMBURGER MENU SLUITEN
+var mainButton = document.querySelector("main");
+
+function klapIn(){
+    let deNav = document.querySelector("nav");
+
+    deNav.classList.remove("toonNav");
+}
+mainButton.addEventListener("click", klapIn);
+// CLOSING THE APP ADVERTISEMENT
+var AppAdKruisje = document.querySelector(".app button");
+var AppSection = document.querySelector(".app");
+
+AppAdKruisje.addEventListener("click", sluitAdvertentie);
+
+function sluitAdvertentie() {
+    AppSection.remove();
+}
+
